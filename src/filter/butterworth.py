ca81486029eea037f3329b1f216fed8c1b195666
Matson <napewnoniematson@gmail.com>
2018-08-13 20:27:19 +0200

    butterworth filter added

---
+++ src/filter/butterworth.py
@@ -0,0 +1,15 @@
+from scipy.signal import butter, lfilter
+
+
+def butter_bandpass(low_cut, high_cut, fs, order=5):
+    nyq = 0.5*fs  # Nyquist
+    low = low_cut/nyq
+    high = high_cut/nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+
+def butter_bandpass_filter(data, low_cut, high_cut, fs, order=5):
+    b, a = butter_bandpass(low_cut, high_cut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
